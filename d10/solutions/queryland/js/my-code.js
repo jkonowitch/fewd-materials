e455bacc4621a7d3c8d1889f30a7fa8a26feb263
Jeffrey Konowitch <jkonowitch@gmail.com>
2017-03-21 20:08:10 -0400

    finished

--- d10/solutions/queryland/js/my-code.js
+++ d10/solutions/queryland/js/my-code.js
@@ -4,16 +4,37 @@ console.log("HELOOOO!");
 // var additionalText = " And I added some stuff.";
 // $("p.cool").text(originalText + additionalText);
 
-var clickCount = 0;
-var originalText = $("p.cool").text();
+// var clickCount = 0;
+// var originalText = $("p.cool").text();
+
+// $("button").on("click", function() {
+//   clickCount = clickCount + 1;
+//   console.log(clickCount);
+//   if (clickCount === 2) {
+//     $("p.cool").text("Woot the button was clicked for the second time!");
+//   } else {
+//     // put it back the way it was
+//     $("p.cool").text(originalText);
+//   }
+// });
+
+// write code such that
+// when a user enters text in the input
+// and presses the button
+// then, that text appears AS the H1 text
 
 $("button").on("click", function() {
-  clickCount = clickCount + 1;
-  console.log(clickCount);
-  if (clickCount === 2) {
-    $("p.cool").text("Woot the button was clicked for the second time!");
-  } else {
-    // put it back the way it was
-    $("p.cool").text(originalText);
-  }
+  var stuffTheUserTyped = $("input").val();
+  $("h1").text(stuffTheUserTyped);
 });
+
+
+
+
+
+
+
+
+
+
+
