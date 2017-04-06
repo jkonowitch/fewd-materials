2f7939c8a1e2d5d4651ddf0090533516dcc52118
Jeffrey Konowitch <jkonowitch@gmail.com>
2017-04-06 19:18:07 -0400

    d15

---
+++ d15/materials/states/js/app.js
@@ -0,0 +1,13 @@
+var ny = {
+  name: "New York",
+  capital: "Albany",
+  governor: "Andrew Cuomo",
+  usSenators: ["Chuck Schumer", "Kirsten Gillibrand"]
+}
+
+var ct = {
+  name: "Connecticut",
+  capital: "Hartford",
+  governor: "Dannel Malloy",
+  usSenators: ["Richard Blumenthal", "Christopher S. Murphy"]
+}
