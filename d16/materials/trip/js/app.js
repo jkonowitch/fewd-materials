eea3ea9e476f274c909af70d0e3f7e4293c47ea9
Jeffrey Konowitch <jkonowitch@gmail.com>
2017-04-11 18:32:46 -0400

    trip

---
+++ d16/materials/trip/js/app.js
@@ -0,0 +1,17 @@
+var trip = [
+  {
+    city: "London",
+    country: "UK",
+    photo: "http://s3.evcdn.com/images/block/I0-001/024/126/410-9.jpeg_/waset-london-2017-conference-10.jpeg"
+  },
+  {
+    city: "Budapest",
+    country: "Hungary",
+    photo: "http://s4.evcdn.com/images/block/I0-001/034/301/859-2.jpeg_/waset-budapest-2017-conference-59.jpeg"
+  },
+  {
+    city: "Lisbon",
+    country: "Portugal",
+    photo: "http://s2.evcdn.com/images/block/I0-001/033/766/357-7.jpeg_/waset-lisbon-2017-conference-57.jpeg"
+  }
+];
